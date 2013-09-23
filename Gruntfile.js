92012ea64112caa60c3add9301543d9f0211ecd0
Nate Murray <nate@natemurray.com>
2013-09-23 08:38:22 -0700

    rm'd level and grunt s3-sync. im just going to use s3cmd

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,14 +1,10 @@
 'use strict';
-
-var level = require('level')
 var lrSnippet = require('grunt-contrib-livereload/lib/utils').livereloadSnippet;
 var mountFolder = function (connect, dir) {
     var directory = require('path').resolve(dir);
     console.log("serving up:", directory)
     return connect.static(directory);
 };
-
-var db = function() { level('./.s3-cache'); };
 
 module.exports = function (grunt) {
   // load all grunt tasks
